1cbef8606153179c9b5e8af55549c2b69d743e03
scamps <scamps1@gmail.com>
2014-10-24 19:26:13 +0200

    0.0.1
    -create project

---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  superfling
+//
+//  Created by Alberto Scampini on 24/10/14.
+//  Copyright (c) 2014 Alberto Scampini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  superfling
+//
+//  Created by Alberto Scampini on 24/10/14.
+//  Copyright (c) 2014 Alberto Scampini. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
