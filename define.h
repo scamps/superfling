9584cacbb100524b1ac5e0ae8732f55696350a2e
scamps <scamps1@gmail.com>
2014-10-24 19:18:15 +0200

    0.0.1 -first commit

---
+++ define.h
@@ -0,0 +1,13 @@
+//
+//  define.h
+//  superfling
+//
+//  Created by Alberto Scampini on 24/10/14.
+//  Copyright (c) 2014 Alberto Scampini. All rights reserved.
+//
+
+#ifndef superfling_define_h
+#define superfling_define_h
+
+
+#endif
